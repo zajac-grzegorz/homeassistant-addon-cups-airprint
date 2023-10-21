6dbdf3b9ebc97b48520884c1339c69fcc232016f
Grzegorz Zajac <74325984+zajac-grzegorz@users.noreply.github.com>
2023-10-21 17:51:33 +0200

    Add persistency for configuration data
    All data is stored in /data/cups folder which maps to volume managed by the supervisor.

--- rootfs/run.sh
+++ rootfs/run.sh
@@ -6,6 +6,12 @@ until [ -e /var/run/avahi-daemon/socket ]; do
   sleep 1s
 done
 
+bashio::log.info "Preparing directories"
+cp -v -R /etc/cups /data
+rm -v -fR /etc/cups
+
+ln -v -s /data/cups /etc/cups
+
 bashio::log.info "Starting CUPS server as CMD from S6"
 
 cupsd -f
